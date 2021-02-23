9c516c3dfdc855bb12617786a3ed8a980119cdef
NefeliTav <msstama2@yahoo.gr>
2021-02-23 16:24:17 +0200

    Add features in shop

--- shop.js
+++ shop.js
@@ -2,3 +2,15 @@ function detectChange(value) {
     var rangeDiv = document.getElementById("output");
     rangeDiv.value = value;
 }
+
+
+/* filter
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+*/
