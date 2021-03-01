1d5ade3638b3beac6515cca101271ef4ecb3b737
NefeliTav <msstama2@yahoo.gr>
2021-03-01 17:40:42 +0200

    Fix shop

--- shop.js
+++ shop.js
@@ -1,6 +1,6 @@
 function detectChange(value) {
-    var rangeDiv = document.getElementById("output");
-    rangeDiv.value = value;
+  var rangeDiv = document.getElementById("output");
+  rangeDiv.value = value;
 }
 
 
@@ -14,3 +14,16 @@ $(document).ready(function(){
   });
 });
 */
+
+function openFilters() {
+  $('.side').css('display', 'block');
+  $('#filters').css('display', 'none');
+  $('#filters2').css('display', 'block');
+}
+
+
+function closeFilters() {
+  $('.side').css('display', 'none');
+  $('#filters2').css('display', 'none');
+  $('#filters').css('display', 'block');
+}
