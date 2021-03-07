ae65c99eb61b81b237d409f8e08758ebf38a3487
lybrag <mirandagioman+github@gmail.com>
2021-03-07 12:18:23 +0200

    Functional Compare Page

---
+++ compare.php
@@ -0,0 +1,358 @@
+<?php
+session_start();
+require_once 'connect.php';
+
+if(isset($_POST['submit']))
+{
+    $_SESSION['failure'] ="";
+    $email = ($_POST['email']);
+    $password = ($_POST['password']);
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+    $result = mysqli_query($conn,"SELECT * FROM users WHERE email='$email'");
+
+    if ($result)
+    {
+
+        if(password_verify($password,$passwordHash)) 
+        {
+            $arr = $result->fetch_array();
+            if ($arr)
+            {
+                /*$_SESSION['id']=$arr[0];*/
+                $_SESSION['firstname']=$arr[0];
+                $_SESSION['lastname']=$arr[1];
+                $_SESSION['email']=$arr[2];
+                $_SESSION['tel']=$arr[3];
+                $_SESSION['address']=$arr[4];
+                $_SESSION['postcode']=$arr[5];
+                $_SESSION['password']=$arr[6];
+            }
+            else
+            {
+                $_SESSION['failure'] = 'Wrong username or password.';
+            }
+        } 
+    }
+    else
+    {
+        $_SESSION['failure'] = 'Wrong username or password.';
+
+    }
+}
+
+$nameErr = $lastnameErr = $emailErr = $telErr = $postcodeErr = $passwordErr = "";
+
+if(isset($_POST['submit2']))
+{
+    
+    $firstname = test_input($_POST["firstname"]);
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$firstname)) 
+    {
+        $nameErr = "Only letters and white space allowed\n";
+    }
+    
+    $lastname = test_input($_POST["lastname"]);
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$lastname)) 
+    {
+        $lastnameErr = "Only letters and white space allowed\n";
+    }
+    
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+    {
+        $emailErr = "Invalid email format\n";
+    }
+    
+    $tel = test_input($_POST["tel"]);
+    if (!preg_match("/^[0-9]{9,11}$/", $tel)) 
+    {
+        $telErr = "Invalid phone format\n";
+    }
+    
+    $address = test_input($_POST["address"]);
+    
+    $postcode = test_input($_POST["postcode"]);
+    if (!preg_match("/^[0-9]{5,10}$/", $postcode)) 
+    {
+        $postcodeErr = "Invalid postcode format\n";
+    }
+    
+    $password = test_input($_POST["password"]);
+    $password2 = test_input($_POST["password2"]);
+    
+    if ($password != $password2) 
+    {
+        $passwordErr = "The two passwords do not match\n";
+    }
+    
+    //echo $firstname,$lastname,$email,$tel,$address,$postcode,$password;
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+    echo $nameErr,$lastnameErr,$emailErr,$telErr,$postcodeErr,$passwordErr;
+
+    $query = "INSERT INTO users (firstname, lastname, email, tel, addr, postcode, psw) VALUES ('$firstname', '$lastname', '$email', '$tel', '$address', '$postcode', '$passwordHash')";
+    $result = mysqli_query($conn,$query);
+    if ($result)
+    {
+        /*$_SESSION['id']=$arr[0];*/
+        $_SESSION['firstname']=$firstname;
+        $_SESSION['lastname']=$lastname;
+        $_SESSION['email']=$email;
+        $_SESSION['tel']=$tel;
+        $_SESSION['address']=$address;
+        $_SESSION['postcode']=$postcode;
+        $_SESSION['password']=$password;
+    }
+
+    
+}
+function test_input($data) 
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>E-Shop</title>
+    <meta charset="utf-8">
+    <!-- responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.5.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!-- link to css -->
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="compare.css">
+    <!-- social media icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- search bar -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+
+
+    <!-- js functions -->
+    <script type="text/javascript" src="index.js"></script>
+
+</head>
+
+<body>
+    <div class="header">
+        <a href="./index.php"><img src="./images/logo.jpg" class="logo"></a>
+        <div class="topnav" id="topnav">
+            <a class="active" href="">Home</a>
+            <a href="#products">Products</a>
+            <a href="#about">About</a>
+            <a href="#photography">Introduction to Photography</a>
+            <a href="#contact">Contact</a>
+
+            <a href="#" class="icon" onclick="openSideNav();return false;">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+        <div class="search-box">
+            <form>
+                <input type="text" class="txt" placeholder="Search Here...">
+                <a class="btn"><i class="fa fa-search"></i></a>
+            </form>
+        </div>
+
+        <a href="#" role="button" title="login" id="login" style="color:black;font-size:20px;width:75px" class="btn btn-success btn-lg">
+            log in
+        </a>
+        <a href="./profile.php" role="button" title="profile" id="profile" style="color:black;font-size:20px;padding-bottom:3.5rem;width:75px"
+            class="btn btn-success btn-lg">
+            profile
+        </a>
+        <a href="" id="cart" title="shopping cart" class="btn btn-success btn-lg">
+            <span class="glyphicon glyphicon-shopping-cart" style="color:black;"></span>
+        </a>
+        <a href="" id="heart" title="wishlist" class="btn btn-success btn-lg">
+            <span style="font-size:18px;background: none;" class="fa">&#xf004;</span>
+        </a>
+
+        <div id="myModal"  class="modal">
+
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <h1 style="font-size:30px;text-align:center;">Log in</h1>
+                <hr>
+                <div class="form-popup">
+
+                    <form action="" method="post" class="form-container">
+                        
+                            <?php 
+                                if (isset($_SESSION['failure']) && ($_SESSION['failure']!="")) {?>
+                                    <div class="failure" style="margin-bottom: 10px;font-size: 18px;color: red;"><?php echo $_SESSION['failure']; ?></div>
+                            <?php }
+                                else
+                                { ?>
+                                    <script>
+                                        document.getElementById("login").style.display = "none";
+                                        document.getElementById("profile").style.display = "block";
+                                    </script>
+                            <?php
+                                }
+                            unset($_SESSION['failure']);
+                            
+                        ?>
+                        
+                        <label for="email" style="font-weight:normal;font-size:20px;">Email</label>
+                        <input type="text" placeholder="Enter Email" name="email" required>
+                        <br>
+                        <label for="psw" style="font-weight:normal;font-size:20px;">Password </label>
+                        <input style="width:20px;height:20px;margin-left: 15em;padding: 0 7em 2em 0;" type="checkbox"
+                            onclick="show_password('myInput')">
+                        Show Password
+
+                        <input type="password" id="myInput" placeholder="Enter Password" name="password" required>
+                        <br>
+
+                        <button name="submit" type="submit" class="btn">Login</button>
+
+                        <p>Don’t have an account?<br><a style="color:black;" href="##" title="signup"
+                                id="signup"><u>Create
+                                    account</u></a></p>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+        <div id="myModal2" class="modal">
+            <div id="modal-content2" class="modal-content">
+                <span id="close" class="close">&times;</span>
+                <h1 style="font-size:30px;text-align:center;">Sign up</h1>
+                <hr>
+                <div class="form-popup">
+                    <form action="./signup.php" method="post" id="form-container2" class="form-container">
+
+                        <label for="firstname" style="font-weight:normal;font-size:20px;">First Name</label>
+                        <label for="lastname" style="margin-left:41%;font-weight:normal;font-size:20px;">Last
+                            Name</label>
+                        <br>
+                        <input type="text" placeholder="Enter first name" name="firstname" required>
+                        <input type="text" style="margin-left:20%;" placeholder="Enter last name" name="lastname" required>
+                        <br>
+                        <label for="email" style="font-weight:normal;font-size:20px;">Email</label>
+                        <label for="tel" style="margin-left:48%;font-weight:normal;font-size:20px;">Phone number</label>
+                        <br>
+                        <input type="email" placeholder="e.g. papadopoulos@gmail.com" name="email" required>
+                        <input type="tel" style="margin-left:20%;" placeholder="e.g.6940234783" name="tel" required>
+                        <br>
+                        <label for="address" style="font-weight:normal;font-size:20px;">Address</label>
+                        <label for="postcode" style="margin-left:45%;font-weight:normal;font-size:20px;">Postcode</label>
+                        <br>
+                        <input type="text" placeholder="Enter Addess" name="address" required>
+                        <input type="text" style="margin-left:20%;" placeholder="Enter Postcode" name="postcode" required>
+                        <br>
+                        <div class="password-field">
+                            <label for="password" style="font-weight:normal;font-size:20px;">Password</label>
+                            <label for="password" style="margin-left:44%;font-weight:normal;font-size:20px;">Confirm</label>
+                            <br>
+                            <input id="myInput1" class="password-field" type="password" placeholder="Enter Password" name="password" required />
+                            <input type="password" style="margin-left:20%;" id="myInput2" placeholder="Confirm Password" onkeyup='check_match();' name="password2" required>
+                        </div>
+                        <span id='message'></span>
+
+
+                        <input type="checkbox" style="margin-left: 60%;padding: 0 7em 2em 0;font-size:10px;width:15px;height:15px;" onclick="show_password('myInput1', 'myInput2')">
+                        Show Password
+                        <br>
+                        <input type="checkbox" style="font-size:20px;width:20px;height:20px;" required> I have read and
+                        i agree to the
+                        Terms &
+                        Conditions
+                        <br>
+                        <button style="padding:10px;" type="submit" name="submit2" class="btn">Sign up</button><br>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="compareGrid">
+        <?php
+        require_once 'connect.php';
+        $_SESSION['email']="test@email.com";
+        $query = "SELECT * FROM products where id in (select id from whishlist where email='" .$_SESSION['email']."' )";
+        $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_array($result)) { ?>
+            <div class="compareGridItem">
+                <div class="card">
+                    <img class="productPhoto" src='<?php echo $row[9]; ?>' alt='<?php echo $row[2]; ?>' style="width:400px; max-height: 300px;">
+                    <hr>
+                    <div class="cardContainer">
+                        <h2><b><?php echo $row[1]; ?></b></h2>
+                        <p class="price"><?php echo $row[8]; ?>$</p>
+                        <p>Resolution (MP): <?php echo $row[3]; ?></p>
+                        <p>Optical Zoom: <?php echo $row[4]; ?></p>
+                        <p>Digital Zoom: <?php echo $row[5]; ?></p>
+                        <p>Screen: <?php echo $row[6]; ?> inch</p>
+                        <input style="margin-left: 10%; margin-right: auto;" type="button" class="buttonCompare" value="Remove">
+                        <input style="margin-left: 7% ; margin-right: auto; margin-top: 10px; margin-bottom: 10px;" type="button" class="buttonCompare" value="Purchase">
+                    </div>
+                </div>
+            </div>
+        <?php }
+        ?>
+    </div>
+
+
+
+
+
+
+    <br>
+    <br>
+    <br>
+    <!-- The overlay -->
+    <div id="myNav" class="overlay">
+
+        <!-- Button to close the overlay navigation -->
+        <a href="javascript:void(0)" class="closebtn" onclick="closeSideNav()">&times;</a>
+
+        <!-- Overlay content -->
+        <div class="overlay-content">
+            <a href="#home" onclick=" closeSideNav()">Home</a>
+            <a href=" #products" onclick="closeSideNav()">Products</a>
+            <a href="#about" onclick="closeSideNav()">About</a>
+            <a href="#photography" onclick="closeSideNav()">Introduction to Photography</a>
+            <a href="#contact" onclick="closeSideNav()">Contact</a>
+
+        </div>
+
+    </div>
+    <hr> <!-- thematic break -->
+    <div class=" footer">
+        <a href="https://www.facebook.com/" title="facebook" class="fa fa-facebook"></a>
+        <a href="https://twitter.com/" title="twitter" class="fa fa-twitter"></a>
+        <a href="https://www.instagram.com/" title="instagram" class="fa fa-instagram"></a>
+        <br>
+        <a href="" style="color:black">Privacy Policy</a>
+        |
+        <a href="" style="color:black">Terms of Use</a>
+        <br>
+        <br>
+        <p style="font-size:15px;">&copy; 2021 G&N , All rights reserved</p>
+
+
+    </div>
+
+
+</body>
+
+</html>
--- e-shop.sql
+++ e-shop.sql
@@ -1,10 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 07, 2021 at 11:11 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-DROP DATABASE `e-shop`;
-CREATE DATABASE `e-shop`;
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `e-shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
 
 CREATE TABLE `messages` (
   `email` varchar(20) NOT NULL,
@@ -12,28 +33,18 @@ CREATE TABLE `messages` (
   `newsletter` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `messages`
+--
+
 INSERT INTO `messages` (`email`, `text`, `newsletter`) VALUES
 ('example@gmail.com', 'I want to ask something about polaroid cameras.', 1);
 
+-- --------------------------------------------------------
 
-CREATE TABLE `users` (
-/*  `id` varchar(10) NOT NULL,*/
-  `firstname` varchar(20) NOT NULL,
-  `lastname` varchar(20) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `tel` varchar(10) NOT NULL,
-  `addr` varchar(30) NOT NULL,
-  `postcode` varchar(10) NOT NULL,
-  `psw` varchar(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-ALTER TABLE `users`
-/*  ADD PRIMARY KEY (`id`);*/
-  ADD PRIMARY KEY (`email`,`psw`); 
-COMMIT;
-
-
+--
+-- Table structure for table `products`
+--
 
 CREATE TABLE `products` (
   `id` varchar(10) NOT NULL,
@@ -45,13 +56,12 @@ CREATE TABLE `products` (
   `screen` float NOT NULL,
   `color` varchar(20) NOT NULL,
   `price` float NOT NULL,
-  `image` varchar(30) NOT NULL
+  `image` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-COMMIT;
-
+--
+-- Dumping data for table `products`
+--
 
 INSERT INTO `products` (`id`, `name`, `brand`, `mp`, `optical`, `digital`, `screen`, `color`, `price`, `image`) VALUES
 ('0', 'Canon Powershot SX530 HS', 'canon', 16, 50, 4, 3, 'black', 269, './images/camera.png'),
@@ -62,14 +72,66 @@ INSERT INTO `products` (`id`, `name`, `brand`, `mp`, `optical`, `digital`, `scre
 ('5', 'Compact Waterproof Leica X-U (Typ 113)', 'leica', 16.2, 0, 0, 3, 'black', 2725.5, './images/camera3.png'),
 ('6', 'Compact Camera Canon PowerShot G3X', 'canon', 20.2, 25, 0, 3, 'black', 749.5, './images/camera4.png'),
 ('7', 'Compact Nikon Coolpix A900', 'nikon', 20.3, 35, 4, 3, 'grey', 278.5, './images/camera7.png'),
-('8', 'Compact Sony Cyber-shot DSC W800', 'sony', 20.1, 5, 10, 2.7, 'grey', 105, './images/camera6.png'),
-('9', 'Compact Camera Canon PowerShot G5X Mark II ', 'canon', 20.1, 5, 4, 3, 'black', 929, './images/camera18.png'),
-('10', 'Compact Nikon Coolpix B600', 'nikon', 16, 60, 0, 3, 'black', 299, './images/camera10.png'),
-('11', 'Compact Camera Canon PowerShot G7X II - Vlogging kit', 'canon', 20.1, 4.2, 0, 3, 'black', 574, './images/camera11.png'),
-('12', 'Sony Cyber-shot DSC-RX100', 'sony', 20.2, 3.6, 0, 3, 'black', 349, './images/camera12.png'),
-('13', 'Sony Cyber-shot DSC-HX60', 'sony', 20.4, 30, 120, 3, 'black', 279, './images/camera13.png'),
-('14', 'Compact Sony DSC - HX80B', 'sony', 20.1, 30, 120, 3, 'black', 349, './images/camera14.png'),
-('15', 'Compact Camera Canon PowerShot G7X II ', 'canon', 20.1, 4.2, 0, 3, 'black', 529, './images/camera15.png'),
-('16', 'Panasonic Lumix Waterpoof DC-FT7EG', 'panasonic', 20.4, 4.6, 6.4, 3, 'red', 299.48, './images/camera16.png'),
-('17', 'Nikon Coolpix P1000', 'nikon', 16, 125, 250, 3.2, 'black', 990, './images/camera17.png');
+('8', 'Compact Sony Cyber-shot DSC W800', 'sony', 20.1, 5, 10, 2.7, 'grey', 105, './images/camera6.png');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `tel` varchar(10) NOT NULL,
+  `addr` varchar(30) NOT NULL,
+  `postcode` varchar(10) NOT NULL,
+  `psw` varchar(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`firstname`, `lastname`, `email`, `tel`, `addr`, `postcode`, `psw`) VALUES
+('Fname', 'Lname', 'test@email.com', '2109898598', 'Test St. 68', '12345', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `whishlist`
+--
+
+CREATE TABLE `whishlist` (
+  `email` varchar(20) NOT NULL,
+  `id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `whishlist`
+--
+
+INSERT INTO `whishlist` (`email`, `id`) VALUES
+('test@email.com', '0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`email`,`psw`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
